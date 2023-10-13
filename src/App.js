bf47532fc93c797750c701b2b91a8e2b9ccdf783
Rahul <rahulk@techechosystem.com>
2023-10-13 15:52:01 +0530

    first changes to code..

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">
-                Home
+                Homes
               </a>
             </li>
             <li className="nav-item">
